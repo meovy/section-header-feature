df846bfb2b712ed01fa44e279914d07607f52a79
ntcbinh <ntcbinh16@gmail.com>
2023-06-13 11:23:28 +0700

    init project & font nucleo

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header__logo">
+        <i className="now-ui-icons business_chart-pie-36"></i>
+      </div>
+    </div>
+  );
+};
+export default Header;
